b88772f5e03da41be7f0a58030b30159384592d4
Nathan Hur <hur.nathan@gmail.com>
2017-07-26 15:26:49 +1200

    :rocket: set up maven and add junit

---
+++ src/main/java/scheduler/Scheduler.java
@@ -0,0 +1,15 @@
+package scheduler;
+
+/**
+ * Entry point to the scheduling algorithm
+ */
+public class Scheduler {
+
+    /**
+     * Command line entry for the algorithm
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/SchedulerTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests to read in problems and verify that the result from the Scheduler is a valid schedule
+ */
+public class SchedulerTests {
+}
