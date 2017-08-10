9501e2f22c87216858d53be92833834b459a24f6
Zihao Yang <zyan781@aucklanduni.ac.nz>
2017-08-10 17:01:07 +1200

    created test case for an invalid solution to test if the validity tests are working properly

--- src/test/java/scheduleValidation/ScheduleValidation.java
+++ src/test/java/scheduleValidation/ScheduleValidation.java
@@ -57,6 +57,7 @@ public class ScheduleValidation {
 
             if (valid[i] == false) {
                 System.out.println("method " + i + " failed");
+
             } else {
                 System.out.println("method" + i + " success");
             }
--- src/test/java/scheduleValidation/ValidationTest.java
+++ src/test/java/scheduleValidation/ValidationTest.java
@@ -1,8 +1,10 @@
 package scheduleValidation;
 
 import algorithm.PartialSolution;
+import algorithm.ProcessorSlot;
 import dotParser.Parser;
 import graph.Graph;
+import graph.Node;
 import org.junit.Before;
 import org.junit.Test;
 import scheduler.InvalidInputException;
@@ -11,6 +13,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.Assert.assertTrue;
@@ -33,7 +37,7 @@ public class ValidationTest {
 
         try {
 
-            String inputFileName = "example2.dot";
+            String inputFileName = "src/test/resources/example.dot";
             if (!inputFileName.endsWith(".dot")) {
                 throw new InvalidInputException("Input file must be dot");
             }
@@ -55,6 +59,69 @@ public class ValidationTest {
             System.out.println("Something went wrong with your input");
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void makeInvalid0and3() {
+        String[] args = new String[2];
+
+        try {
+
+            String inputFileName = "src/test/resources/testInvalid.dot";
+            if (!inputFileName.endsWith(".dot")) {
+                throw new InvalidInputException("Input file must be dot");
+            }
+            args[0] = inputFileName;
+            File inputFile = new File(inputFileName);
+
+            args[1] = "2";
+
+            _graph = Parser.parseDotFile(inputFile);
+
+        } catch (InvalidInputException e) {
+            System.out.println();
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Something went wrong with your input");
+            e.printStackTrace();
+        }
+
+        List<Node> nodes = _graph.getNodes();
+        Node nodeA = null;
+        Node nodeB = null;
+        Node nodeC = null;
+        Node nodeD = null;
+        for (Node node: nodes) {
+            if (node.getName() == "a") {
+                nodeA = node;
+            } else if (node.getName() == "b") {
+                nodeB = node;
+            } else if (node.getName() == "c") {
+                nodeC = node;
+            } else if (node.getName() == "d") {
+                nodeD = node;
+            }
+        }
+        PartialSolution invalidSolution = new PartialSolution(2);
+
+        ProcessorSlot slotA = new ProcessorSlot(nodeA, 8, 10, 2);
+        ProcessorSlot slotB = new ProcessorSlot(nodeB, 0, 3, 1);
+        ProcessorSlot slotC = new ProcessorSlot(nodeC,10, 13, 2);
+        ProcessorSlot slotD = new ProcessorSlot(nodeD,12, 14, 2);
+
+        ArrayList<ProcessorSlot> processor1 =  new ArrayList<ProcessorSlot>();
+        processor1.add(slotB);
+
+        ArrayList<ProcessorSlot> processor2 =  new ArrayList<ProcessorSlot>();
+        processor2.add(slotA);
+        processor2.add(slotC);
+        processor2.add(slotD);
+
+        invalidSolution._processors[0] = processor1;
+        invalidSolution._processors[1] = processor2;
+
+
+        assertTrue(ScheduleValidation.scheduleIsValid(_graph, invalidSolution));
     }
 
     @Test
