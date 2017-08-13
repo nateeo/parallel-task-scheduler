6a3f00399c5c413de10913bf381921d4762ad40a
Samule Li <s.li@crimsoneducation.org>
2017-08-14 08:54:11 +1200

    set up for Javafx frontend

--- src/main/java/frontend/main.java
+++ src/main/java/frontend/main.java
@@ -3,14 +3,26 @@ package frontend;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.File;
+import graph.Graph;
+
+import static dotParser.Parser.parseDotFile;
+
 public class main extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    private File testGraph = new File ("../../../test/resources/exampleSmall.dot");
+    private Graph graph = parseDotFile(testGraph);
+
     @Override
     public void start(Stage primaryStage) {
+
+    }
+
+    public void addGraph() {
 
     }
 }
