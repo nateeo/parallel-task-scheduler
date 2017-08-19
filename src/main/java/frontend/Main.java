02ae561156d4cc0392c7b4cf8a81b0bd7ccf3325
Samule Li <s.li@crimsoneducation.org>
2017-08-19 13:52:30 +1200

    staged Files

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -1,28 +1,33 @@
 package frontend;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.net.URL;
+
 import graph.Graph;
 
 import static dotParser.Parser.parseDotFile;
 
-public class main extends Application {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = new File("src/main/java/frontend/title.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("Welcome to Hi-5 Scheduling");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    private File testGraph = new File ("../../../test/resources/exampleSmall.dot");
-    private Graph graph = parseDotFile(testGraph);
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
-
-    public void addGraph() {
-
-    }
+    //private File testGraph = new File ("../../../test/resources/exampleSmall.dot");
+    //private Graph graph = parseDotFile(testGraph);
 }
--- src/main/java/frontend/TitleController.java
+++ src/main/java/frontend/TitleController.java
@@ -1,4 +1,50 @@
 package frontend;
 
-public class TitleController {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TitleController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void handleNextButtonClick(ActionEvent actionEvent) throws Exception {
+        changeScene("src/main/java/frontend/LoadingPage.fxml",actionEvent);
+    }
+
+    @FXML
+    public void handleCloseButton() {
+        Boolean closeOperation = DialogBox.displayConfirmDialogBox("Exit","Are you sure you want to close?");
+        if (closeOperation) {
+            Platform.exit();
+        }
+    }
+
+    @FXML
+    public void handleSettingsButton(ActionEvent actionEvent) throws Exception{
+        changeScene("src/main/java/frontend/Settings.fxml",actionEvent);
+    }
+
+    public void changeScene(String filePathToXML, ActionEvent actionEvent) throws Exception{
+        URL url = new File(filePathToXML).toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene nextScene = new Scene(root);
+        Stage loadingPage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        loadingPage.setScene(nextScene);
+    }
 }
