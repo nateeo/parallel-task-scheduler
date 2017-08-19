e72421af9a61251c408ac01637dfb15b40deaf91
Samule Li <s.li@crimsoneducation.org>
2017-08-19 15:27:40 +1200

    can draw rectangle

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -1,5 +1,6 @@
 package frontend;
 
+import graph.Node;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 
 import graph.Graph;
 
@@ -15,13 +17,25 @@ import static dotParser.Parser.parseDotFile;
 
 public class Main extends Application {
 
+    static Graph _graph;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // input the graph.
+        File testGraph = new File ("src/test/resources/exampleSmall.dot");
+        _graph = parseDotFile(testGraph);
+        List<Node> listOfNodes = _graph.getNodes();
+
         URL url = new File("src/main/java/frontend/SplashScreen.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Welcome to Hi-5 Scheduling");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static Graph getGraph(){
+        return _graph;
     }
 
     public static void main(String[] args) {
--- src/main/java/frontend/SplashScreen.java
+++ src/main/java/frontend/SplashScreen.java
@@ -1,5 +1,7 @@
 package frontend;
 
+import frontend.Main;
+import graph.Graph;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,19 +9,30 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
 
 import javafx.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class SplashScreen implements Initializable {
+
+    Graph _graph;
+
+    @FXML
+    private Canvas canvas;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        _graph = Main.getGraph();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.strokeRect(100,100,300,300);
     }
 
     public void changeScene(String filePathToXML, ActionEvent actionEvent) throws Exception{
