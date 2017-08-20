4513f4b1d318df3742859750e4ca4acea4ae2963
Samule Li <s.li@crimsoneducation.org>
2017-08-20 15:46:32 +1200

    Can draw in the nodes

--- src/main/java/frontend/SplashScreen.java
+++ src/main/java/frontend/SplashScreen.java
@@ -1,45 +1,72 @@
 package frontend;
 
-import frontend.Main;
+import graph.Edge;
 import graph.Graph;
-import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Modality;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
-import javafx.scene.canvas.Canvas;
 
-import javafx.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
 public class SplashScreen implements Initializable {
 
     Graph _graph;
+    Map<String, Circle> shapeMap = new HashMap<String, Circle>();
 
     @FXML
-    private Canvas canvas;
+    private AnchorPane graphPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         _graph = Main.getGraph();
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.strokeRect(100,100,300,300);
+        drawGraph(_graph);
+
     }
 
     public void changeScene(String filePathToXML, ActionEvent actionEvent) throws Exception{
         URL url = new File(filePathToXML).toURI().toURL();
         Parent root = FXMLLoader.load(url);
         Scene nextScene = new Scene(root);
-        Stage loadingPage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Stage loadingPage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
         loadingPage.setScene(nextScene);
+    }
+
+    public void drawGraph(Graph graph) {
+        List<graph.Node> nodes = graph.getNodes();
+        List<Edge> edges = graph.getEdges();
+        int increment = 10;
+
+        for(graph.Node node: nodes) {
+            System.out.println(node.getName());
+            Circle circle = new Circle();
+            circle.setCenterX(30);
+            circle.setCenterY(30);
+            circle.setRadius(15);
+            circle.setFill(Color.WHITE);
+            circle.setStroke(Color.BLACK);
+            StackPane stackPane = new StackPane();
+            Text text = new Text(node.getName());
+            text.setBoundsType(TextBoundsType.VISUAL);
+            stackPane.getChildren().addAll(circle,text);
+            graphPane.getChildren().add(stackPane);
+            stackPane.setLayoutX(increment);
+            increment = increment + 40;
+        }
     }
 }
